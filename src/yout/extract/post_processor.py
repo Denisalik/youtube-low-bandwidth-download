8f30a4ded60d4e913c22a9b443b40f086337c6a6
Denisalik <schegletovdenis@mail.ru>
2026-09-26 17:56:01 +0300

    shorts info dict doesn't have filesize, fix it

--- src/yout/extract/post_processor.py
+++ src/yout/extract/post_processor.py
@@ -2,14 +2,16 @@ from yt_dlp.postprocessor.common import PostProcessor
 
 from src.entity.file import File, FileState
 
-
 class StorageInfoPP(PostProcessor):
     def __init__(self, file: File, downloader=None):
         super().__init__(downloader)
         self.file = file
 
     def run(self, info: dict) -> tuple[list, dict]:
-        self.file.size = info["filesize"]
+        #check dict if needed
+        # with open("/data/files/output.json", "w", encoding="utf-8") as f:
+        #     json.dump(info, f, indent=4, sort_keys=True, default=str)
+        self.file.size = info.get("filesize", info.get("filesize_approx"))
         self.file.channel_name = info["channel"]
         self.file.name = info["title"]
         self.file.duration = info["duration_string"]
